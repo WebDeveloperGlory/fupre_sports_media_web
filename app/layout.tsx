69bf0f93d1065f19a27a4152acd3122005e285f0
WebDeveloperGlory <gloryumuerri223@gmail.com>
2025-02-16 00:23:02 -0800

    add analytics and test something on homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // app/layout.tsx
 import '@/assets/styles/globals.css';
 import { Space_Grotesk } from 'next/font/google';
+import { Analytics } from "@vercel/analytics/react"
 import Navbar from '@/components/Navbar';
 import { ThemeProvider } from '@/providers/theme-provider';
 import { LoadingProvider } from "@/providers/loading-provider";
@@ -42,9 +43,10 @@ export default async function RootLayout({
               <AuthWrapper data={ availableCookies } />
               <LoadingOverlay />
               <NavigationEvents />
-        <Navbar />
+          <Navbar />
               <main className="px-4 pt-8 pb-20 md:px-6 md:pt-24 md:pb-6">
         {children}
+        <Analytics />
               </main>
             </ToastProvider>
           </ThemeProvider>
